865873c99dbf56875c29daa15908c50a51ccbbf1
Raphael_Laurent <raphael.laurent@cenova.fr>
2021-01-12 23:06:03 +0100

    main app update

--- main_app.py
+++ main_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 
 from dl_audio_subs_from_yt import *
+from api import *
+
+
+AUDIO_PATH = '/Audios_files/'
 
 st.title('Projet PAS, reconnaissance vocale')
 
@@ -11,8 +15,10 @@ yt_link = st.text_input("URL Youtube :", 'https://youtu.be/g0Q5YeZ4YOA')
 
 if st.button('Valider'):
 
-	titre = download(yt_link)
-	st.write("""Téléchargement de la vidéo et de ses sous-titre terminé. """)
+	audio_file_name, subs_file_name = download(yt_link)
+	st.write("""Téléchargement de la vidéo :""" + audio_file_name + """et de ses sous-titres terminé. """)
 
+if st.button('Appeler les API'):
+	WatsonApi.transcribe(AUDIO_PATH + audio_file_name)
 
 
